c8996aadc6f7ad2d77bddf377adb02deef3c13bc
Artem-web-js <zenticul@gmail.com>
2023-07-02 20:26:07 +0300

    init commit

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { useParams, useRouter } from "next/navigation";
+
+import { Plus } from "lucide-react";
+
+import { Heading } from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
+
+import { BillboardColumn, columns } from "./columns";
+
+export const BillboardClient = ({ data }: { data: BillboardColumn[] }) => {
+  const router = useRouter();
+  const params = useParams();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Billboards (${data.length})`}
+          description="Manage billboards for your store"
+        />
+        <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
+          <Plus className="mr-2 w-4 h-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable
+        searchKey="label"
+        columns={columns}
+        data={data}
+      />
+      <Heading
+        title="API"
+        description="API calls for Billboards"
+      />
+      <Separator />
+      <ApiList entityName="billboards" entityIdName="billboardId" />
+    </>
+  );
+};
