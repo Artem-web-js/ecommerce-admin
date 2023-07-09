5796861b73e4e23cabc60c1c68dceaf8446c644c
Artem-web-js <zenticul@gmail.com>
2023-07-09 18:56:11 +0300

    added overview entity

---
+++ components/overview.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import {
+  Bar,
+  BarChart,
+  ResponsiveContainer,
+  XAxis,
+  YAxis
+} from "recharts";
+
+export const Overview = ({ data }: { data: any[] }) => {
+  return (
+    <ResponsiveContainer width="100%" height={350}>
+      <BarChart data={data}>
+        <XAxis
+          dataKey="name"
+          stroke="#888888"
+          fontSize={12}
+          tickLine={false}
+          axisLine={false}
+        />
+        <YAxis
+          stroke="#888888"
+          fontSize={12}
+          tickLine={false}
+          axisLine={false}
+          tickFormatter={(value: string) => `$${value}`}
+        />
+        <Bar dataKey="total" fill="#3498db" radius={[4, 4, 0, 0]} />
+      </BarChart>
+    </ResponsiveContainer>
+  )
+};
